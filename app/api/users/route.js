45f6cf71ceed067cd3b9e3d4039b55711ed2e2c0
Mohammd-AzmatJaved <tjavemohd@gmail.com>
2026-04-11 16:55:12 +0530

    Add console log for variable 'x' in route.js
    Added a console log for variable 'x' in GET function.

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -5,6 +5,7 @@ export async function GET() {
   const { data, error } = await supabase.from("users").select("*");
   return NextResponse.json(data);
 }
+let x
 console.log(x)
 export async function POST(req) {
   const body = await req.json();

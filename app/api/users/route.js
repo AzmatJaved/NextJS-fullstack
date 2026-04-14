e7b68d6f0c5956f6e0f9c4fe1310740e9aae9645
AzmatJaved <50517403+AzmatJaved@users.noreply.github.com>
2026-04-14 16:20:37 +0530

    Fix syntax error in users routedf
    Removed a line of code that appears to be an error.

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,6 +1,6 @@
 import { supabase } from "@/app/lib/supabase";
 import { NextResponse } from "next/server";
-
+kdjlk;jf
 export async function GET() {
   const { data, error } = await supabase.from("users").select("*");
   return NextResponse.json(data);

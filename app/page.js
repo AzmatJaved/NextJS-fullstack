efa46edb0d0756c1cf6d4ca3efc741d89ad7cbcc
AzmatJaved <50517403+AzmatJaved@users.noreply.github.com>
2026-04-14 12:14:34 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-
+jklj
 export default function Home() {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState("");

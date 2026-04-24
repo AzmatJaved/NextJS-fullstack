b52242f34b0a241c98578cdbf6230af656c4e023
Mohammad Azmat Javed <50517403+AzmatJaved@users.noreply.github.com>
2026-04-24 12:40:01 +0530

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,6 +1,6 @@
 
 import { NextResponse } from "next/server";
-
+console.log("hello")
   export async function GET() {
   const { data, error } = await supabase.from("users").select("*");
   return NextResponse.json(data);
